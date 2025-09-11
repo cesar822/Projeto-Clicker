35d23015c87b26b56049320dda5b1aa8684fb19e
César Costa <62480657+cesar822@users.noreply.github.com>
2025-09-11 10:23:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+import { Start } from './scenes/Start.js';
+import { Menu } from './scenes/Menu.js'
+import { TelaInicial } from './scenes/TelaInicial.js'
+
+const config = {
+    type: Phaser.AUTO,
+    title: 'Overlord Rising',
+    description: '',
+    parent: 'game-container',
+    width: 1280,
+    height: 720,
+    backgroundColor: '#000000',
+    pixelArt: false,
+    scene: [
+        TelaInicial,
+        Start,
+        Menu
+    ],
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+}
+
+new Phaser.Game(config);
+            
